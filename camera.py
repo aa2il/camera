71b1dbc1a98a12a63768c860c3af3578049a2f60
aa2il <jattili@yahoo.com>
2024-05-01 10:19:54 -0700

    Initial commit

---
+++ camera.py
@@ -0,0 +1,213 @@
+#! /usr/bin/python3 -u
+############################################################################################
+#
+# Camera - Rev 0.1
+# Copyright (C) 2024 by Joseph B. Attili, aa2il AT arrl DOT net
+#
+# Simple app to view and control Tapo C500 Video Camera.
+#
+############################################################################################
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+############################################################################################
+
+import sys
+import cv2
+from tapoC500 import TapoC500
+from time import time
+from settings import CONFIG_PARAMS
+
+############################################################################################
+
+class VIDEO_CAMERA():
+    def __init__(self,P):
+
+        self.P=P
+        self.step=0.1
+
+        IP=P.SETTINGS['MY_IP']
+        USER=P.SETTINGS['MY_USER']
+        PASSWORD=P.SETTINGS['MY_PASSWORD']
+
+        self.cross_hairs=None
+        self.cross_size=50
+        self.box1=None
+        self.box2=None
+        self.LEFT_DOWN=False
+        
+        # See opencv.org for details
+        print('Setting up Video Camera - Press ESCAPE to exit ...')
+
+        # Open PTZ controls
+        self.ptz = TapoC500(IP,USER,PASSWORD)
+
+        # Open camera video stream
+        print('Opening video stream ...',self.ptz.RTSP_URL)
+        self.cap = cv2.VideoCapture(self.ptz.RTSP_URL)
+
+        print('And away we go!')
+        self.run_camera()
+
+    # Read the stream
+    def run_camera(self):
+        
+        while(self.cap.isOpened()):
+            ret, img = self.cap.read()
+    
+            # Put frame operations here - e.g. resize
+            #print('Original Dimensions : ',img.shape)
+            sc = .75 # 1 # 0.5
+            width = int(sc*img.shape[1])
+            height = int(sc*img.shape[0])
+            dim = (width, height)
+            self.img2 = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+            #print('Resized Dimensions : ',img2.shape)
+
+            # Add box/cross-hairs
+            if self.LEFT_DOWN:
+                self.DrawBox()
+                self.cross_hairs=None
+            else:
+                if self.cross_hairs:
+                    self.DrawCrossHairs()
+                    
+            # Display the frame
+            cv2.imshow('frame', self.img2)
+
+            # Enable mouse callback
+            cv2.setMouseCallback('frame', self.MouseCB)
+            
+            # Check for clean exit
+            ch = cv2.waitKey(20) # & 0xFF
+            if ch>0:
+                print('ch=',ch)
+                if ch==82:
+                    self.ptz.rel_move(0,-self.step)
+                elif ch==84:
+                    self.ptz.rel_move(0,self.step)
+                elif ch==81:
+                    self.ptz.rel_move(-self.step,0)
+                elif ch==83:
+                    self.ptz.rel_move(self.step,0)
+                elif ch in [27,ord('q'),ord('Q')]:
+                    break
+            
+        # Clean-up and quit
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+    def MouseCB(self,event, x, y, flags, param):
+
+        if event == cv2.EVENT_MOUSEMOVE:
+            #print(cross_hairs)
+        
+            imgCopy = self.img2.copy()
+            #cv2.circle(imgCopy, (x, y), 10, (255, 0, 0), -1)
+            if self.LEFT_DOWN:
+                self.box2=(x,y)
+                #DrawBox(imgCopy,box1,box2)
+            else:
+                self.cross_hairs=(x,y)
+                #DrawCrossHairs(imgCopy,cross_hairs,cross_size)
+            cv2.imshow('frame', imgCopy)
+
+        elif event == cv2.EVENT_LBUTTONDBLCLK:
+
+            print('MouseCB: Left Button DOUBLE CLOCK:',x, y, flags, param)
+            w=self.img2.shape[1]
+            h=self.img2.shape[0]
+            center=( int(w/2), int(h/2) )
+            lr = 2*self.ptz.ifov1*(x-center[0])/w
+            ud = 2*self.ptz.ifov2*(y-center[1])/h
+            print('center=',center,'lr=',lr,'ud=',ud)
+            self.ptz.rel_move(lr,ud)
+        
+        elif event == cv2.EVENT_LBUTTONDOWN:
+
+            print('MouseCB: Left Button DOWN:',x, y, flags, param)
+            self.LEFT_DOWN=True
+            self.box1=(x,y)
+            self.box2=self.box1
+            #t1=time()
+            #print(t1)
+    
+        elif event == cv2.EVENT_LBUTTONUP:
+
+            print('MouseCB: Left Button UP:',x, y, flags, param)
+            self.LEFT_DOWN=False
+            self.box2=(x,y)
+            #t2=time()
+            #print(t2)
+
+            if self.box2==self.box1:
+                print('Quick click!')
+
+        elif event==cv2.EVENT_MOUSEWHEEL:
+
+            print('MouseCB: WHEEL:',x, y, flags, param)
+            if flags>0:
+                print('Zoom in')
+            else:
+                print('Zoom out')
+                
+
+    def DrawBox(self):
+    
+        p1 = (self.box1[0],self.box1[1])
+        p2 = (self.box1[0],self.box2[1])
+        p3 = (self.box2[0],self.box2[1])
+        p4 = (self.box2[0],self.box1[1])
+        #print(p1,p2,p3,p4)
+    
+        c  =(0,0,255)  # Red
+        th = 2         # Line thickness
+    
+        cv2.line(self.img2, p1, p2, c, thickness=th)
+        cv2.line(self.img2, p2, p3, c, thickness=th)
+        cv2.line(self.img2, p3, p4, c, thickness=th)
+        cv2.line(self.img2, p4, p1, c, thickness=th)
+
+    def DrawCrossHairs(self):
+    
+        # If mouse hasn't moved yet, put cross-hairs in the center of the image
+        #print(cross_hairs)
+        if self.cross_hairs==None:
+            center=( int(img.shape[1]/2), int(img.shape[0]/2) )
+        else:
+            center=self.cross_hairs
+
+        p1 = (center[0], center[1] - self.cross_size)
+        p2 = (center[0], center[1] + self.cross_size)
+        p3 = (center[0] - self.cross_size, center[1])
+        p4 = (center[0] + self.cross_size, center[1])
+        #print(p1,p2,p3,p4)
+
+        c  =(0,0,255)  # Red
+        th = 2         # Line thickness
+    
+        cv2.line(self.img2, p1, p2, c, thickness=th)
+        cv2.line(self.img2, p3, p4, c, thickness=th)
+
+############################################################################################
+        
+if __name__ == '__main__':
+
+    # Handle resource file (settings)
+    ATTRS=['IP','USER','PASSWORD']
+    P=CONFIG_PARAMS('.camera1rc',ATTRS)
+    print(P)
+    print(P.SETTINGS)
+    
+    camera=VIDEO_CAMERA(P)
+    
+    print("\nThat's All Folks!\n")
+
---
+++ tapoC500.py
@@ -0,0 +1,255 @@
+#! /usr/bin/python3 -u
+############################################################################################
+#
+# tapoC500.py - Rev 0.1
+# Copyright (C) 2024 by Joseph B. Attili, aa2il AT arrl DOT net
+#
+# PTZ control for Tapo C500 Camera.
+#
+# pip3 install onvif_zeep
+# pip3 install opencv-python
+#
+# Useful websites for determining how to do this:
+# https://www.onvif.org/onvif/ver20/util/operationIndex.html
+# https://pkg.go.dev/github.com/use-go/onvif/ptz#pkg-functions
+# https://github.com/quatanium/python-onvif
+# https://github.com/RichardoMrMu/python-onvif
+# https://carlrowan.wordpress.com/2018/12/23/ip-camera-control-using-python-via-onvif-for-opencv-image-processing/
+# https://www.mjr19.org.uk/IT/seminar_cam/ptz.html
+#
+############################################################################################
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+############################################################################################
+
+import sys
+from time import sleep
+import datetime
+from onvif import ONVIFCamera
+
+############################################################################################
+
+class TapoC500():
+    def __init__(self,IP,USER,PASSWORD,PORT=2020):
+
+        # Camera params
+        self.RTSP_URL = "rtsp://"+USER+":"+PASSWORD+"@"+IP+":554/stream1"
+        self.ifov1=4.5/20       # 2 is a complete revolution (360-deg) and it takes about 4.5/20 steps to move an object from one side to the other (one FOV)
+        self.ifov2=10/20         # A step of 2 is needed to go from max up to max down (114-deg) and about 10/20 steps to move an object from top to bottome (one FOV)
+        
+        # Open camera control port
+        self.mycam = ONVIFCamera(IP,PORT,USER,PASSWORD)
+
+        # Get Hostname
+        resp = self.mycam.devicemgmt.GetHostname()
+        print('Camera hostname= ' + str(resp.Name))
+
+        # Get date and time
+        dt = self.mycam.devicemgmt.GetSystemDateAndTime()
+        tz = dt.TimeZone
+        year = dt.UTCDateTime.Date.Year
+        hour = dt.UTCDateTime.Time.Hour
+        min  = dt.UTCDateTime.Time.Minute
+        print('Camera date/time: tz=',tz,'\tyear=',year,'\thour=',hour,'\tmin=',min)
+        
+        # Create media service object
+        self.media = self.mycam.create_media_service()
+        self.media_profile = self.media.GetProfiles()[0]
+        self.token = self.media_profile.token
+        
+        # Create ptz service object & check what services are available
+        self.ptz = self.mycam.create_ptz_service()
+        request = self.ptz.create_type('GetServiceCapabilities')
+        Service_Capabilities = self.ptz.GetServiceCapabilities(request)
+        print('Service_Capabilities=',Service_Capabilities)
+
+        # GetStatus doesn't work - ugh!
+        #status = self.ptz.GetStatus({'ProfileToken': self.token})
+
+        # Get PTZ configuration options
+        request = self.ptz.create_type('GetConfigurationOptions')
+        request.ConfigurationToken = self.media_profile.PTZConfiguration.token
+        ptz_configuration_options = self.ptz.GetConfigurationOptions(request)
+        print('\nrequest=',request)
+        print('\nptz config options=',ptz_configuration_options)
+
+        # Setup continuousMove request
+        self.requestCont = self.ptz.create_type('ContinuousMove')
+        self.requestCont.ProfileToken = self.media_profile.token
+        #self.requestc.Velocity = self.ptz.GetStatus({'ProfileToken': self.media_profile.token}).Position
+        self.requestCont.Velocity = {'PanTilt': {'x': 0, 'y': 0} , 'Zoom': {'x':0} }
+
+        # Setup absoluteMove request
+        self.requestAbs = self.ptz.create_type('AbsoluteMove')
+        self.requestAbs.ProfileToken = self.media_profile.token
+        self.requestAbs.Position = {'PanTilt': {'x': 0, 'y': 0} ,'Zoom': 0 }
+
+        # Setup relativeMove request
+        self.requestRel = self.ptz.create_type('RelativeMove')
+        self.requestRel.ProfileToken = self.media_profile.token
+        self.requestRel.Translation = {'PanTilt': {'x': 0, 'y': 0} ,'Zoom': 0 }
+
+        # Setup other requests
+        self.requests = self.ptz.create_type('Stop')
+        self.requests.ProfileToken = self.media_profile.token
+        self.requestp = self.ptz.create_type('SetPreset')
+        self.requestp.ProfileToken = self.media_profile.token
+        self.requestg = self.ptz.create_type('GotoPreset')
+        self.requestg.ProfileToken = self.media_profile.token
+        self.stop()
+
+        # This shows how to parse various config options - should use thse eventually
+        XMIN = ptz_configuration_options.Spaces.AbsolutePanTiltPositionSpace[0].XRange.Min
+        XMAX = ptz_configuration_options.Spaces.AbsolutePanTiltPositionSpace[0].XRange.Max
+        YMIN = ptz_configuration_options.Spaces.AbsolutePanTiltPositionSpace[0].YRange.Min
+        YMAX = ptz_configuration_options.Spaces.AbsolutePanTiltPositionSpace[0].YRange.Max
+        print('\nXMIN=',XMIN,'\tXMAX=',XMAX,'\tYMIN=',YMIN,'\tYMAX=',YMAX)
+
+        # Create recording service object - doesn't work!
+        #self.rec = self.mycam.create_recording_service()
+        #self.rec = self.mycam.create_search_service()
+        #self.rec = self.mycam.create_replay_service()
+        #self.events = self.mycam.create_events_service()
+        #e = self.events.GetEventProperties()
+        #print(e)
+        #e = self.events.GetServiceCapabilities()
+        #print(e)
+        #e = self.events.PullMessages()
+        #print(e)
+        #profiles = self.media.GetProfiles()
+        #print('\nprofiles=',profiles)
+        #uri = self.media.GetSnapshotUri({'ProfileToken': self.media_profile.token})
+        #print('\nuri=',uri)
+        #caps = self.mycam.devicemgmt.GetCapabilities()
+        #print('\ncaps=',caps)
+        #sys.exit(0)
+
+    # Relative move
+    def rel_move(self,x,y,z=0):
+
+        # Set position & speed
+        self.requestRel.Translation = {'PanTilt': {'x': x, 'y': y} ,'Zoom': z }
+        #request.Speed    = {'PanTilt': {'x': XMAX, 'y': YMAX}}
+        #print('\nrequest=',request)
+    
+        # Send the command
+        try:
+            self.ptz.RelativeMove(self.requestRel)
+        except:
+            print('Relative Move Failed - probably hit stop')
+
+    # Absolute move
+    def abs_move(self,x,y,z=0):
+        
+        # Set position & speed
+        self.requestAbs.Position = {'PanTilt': {'x': x, 'y': y} ,'Zoom': z }
+        #request.Speed    = {'PanTilt': {'x': XMAX, 'y': YMAX}}
+        #print('\nrequest=',request)
+    
+        # Send the command
+        self.ptz.AbsoluteMove(self.requestAbs)
+
+    # Continuous move
+    def cont_move(self,dir,timeout=1):
+        
+        # Should read these from the camera but good senough for now ...
+        XMAX = 1
+        XMIN = -1
+        YMAX = 1
+        YMIN = -1
+
+        # Set Pan-Tilt movement
+        if dir=='up':
+            x=0
+            y=YMAX
+            z=0
+        elif dir=='down':
+            x=0
+            y=YMIN
+            z=0
+        elif dir=='left':
+            x=XMAX
+            y=0
+            z=0
+        elif dir=='right':
+            x=XMIN
+            y=0
+            z=0
+        elif dir=='in' and False:
+            # It doesn't look like zooming is available on this camera
+            x=0
+            y=0
+            z=1
+        elif dir=='out' and False:
+            x=0
+            y=0
+            z=-1
+        else:
+            print('\n*** ERROR *** Invalid direction',dir)
+            return
+        self.requestCont.Velocity = {'PanTilt': {'x': x, 'y': y} , 'Zoom': {'x':z} }
+        
+        # Set the timeout for the movement
+        self.requestCont.Timeout = datetime.timedelta(seconds=timeout)
+        #print('\nrequest=',request)
+        
+        # Send the command
+        try:
+            self.ptz.ContinuousMove(self.requestCont)
+        except:
+            print('Continuous Move Failed - probably hit stop')
+    
+    # Stop pan, tilt and zoom
+    def stop(self):
+        self.requests.PanTilt = True
+        self.requests.Zoom = True
+        print(f"self.request:{self.requests}")
+        self.ptz.Stop(self.requests)
+
+############################################################################################
+
+if __name__ == '__main__':
+    ptz = TapoC500(IP,PORT,USER,PASS)
+    
+    #ptz.rel_move(0.1,0.1)
+    ptz.abs_move(0.,0.)
+    sleep(2)
+
+    if True:
+        ptz.cont_move('left')
+        sleep(2)
+        ptz.cont_move('up')
+        sleep(2)
+        ptz.cont_move('right')
+        sleep(2)
+        ptz.cont_move('down')
+
+    # These don't work
+    if False:
+        request = ptz.ptz.create_type('GotoHomePosition')
+        request.ProfileToken = ptz.media_profile.token
+        request.Speed = {'PanTilt': {'x': 0.5, 'y': 0.5}}
+        print('\nrequest=',request)
+        status = ptz.ptz.GotoHomePosition(request)
+        print('\nstatus=',status)
+        sys.exit(0)
+        
+    if False:
+        request = ptz.ptz.create_type('GetStatus')
+        request.ProfileToken = ptz.media_profile.token
+        print('\nrequest=',request)
+        status = ptz.ptz.GetStatus(request)
+        print('\nstatus=',status)
+        sys.exit(0)
+    
+
+        
